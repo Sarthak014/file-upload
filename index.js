6cd6f376063bc4cdf8fdfbf34e8f2dc5ea04cebc
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-08-15 11:36:21 +0530

    feat: add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var cors = require('cors');
+const multer  = require('multer');
+require('dotenv').config()
+
+var app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Create a POST endpoint for '/upload' route
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
