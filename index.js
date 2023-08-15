cfc38228f1a337af77c8704b4a6732ea55b928a8
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-08-15 12:33:08 +0530

    feat: update index.js with mongoose connect, db controller method call

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ require('dotenv').config()
 var app = express();
 const upload = multer({ dest: '/public/' }).single("upfile");
 
+// import Mongoose
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -13,6 +22,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const { saveFileDetails } = require('./controller/fileUpload');
 // Create a POST endpoint for '/api/fileanalyse' route
 app.post("/api/fileanalyse", (req, res, next) => {
   upload(req, res, function (err) {
@@ -22,14 +32,44 @@ app.post("/api/fileanalyse", (req, res, next) => {
       console.error('Unknown File Upload Error: ', err);
     }
     console.log('File request is: ', req.file);
-    const { originalname, mimetype, size } = req.file;
-    res.json({ name: originalname, type: mimetype, size });
-
-    next();
+    saveFileDetails(req.file, function (err, data) {
+      if (err) {
+        return next(err);
+      }
+      if (!data) {
+        console.log("Missing `done()` argument");
+        return next({ message: "Missing callback argument" });
+      }
+  
+      return res.json(data);
+    });
   });
 });
 
+// Error handler
+app.use(function (err, req, res, next) {
+  if (err) {
+    res
+      .status(err.status || 500)
+      .type("txt")
+      .send(err.message || "SERVER ERROR");
+  }
+});
 
+// Mongoose Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error("Database connection error");
+  });
+
+// Server Connection
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
